5db25b4584a63abc296bafddff9861097976674b
LAMA BISHAL <lb1627565@gmail.com>
2025-08-12 03:17:23 +0900

    coookie and redirects in route handlers

--- app/profile/api/route.js
+++ app/profile/api/route.js
@@ -1,5 +1,6 @@
 import { NextRequest } from 'next/server';
 import { headers } from 'next/headers';
+import { cookies } from 'next/headers';
 
 export async function GET( NextRequest) {
   // const requestHeaders = new Headers(NextRequest.headers)
@@ -7,9 +8,28 @@ export async function GET( NextRequest) {
 
     const headersList = await headers();
     console.log( headersList.get('Authorization'));
+
+  const theme = NextRequest.cookies.get('theme');
+  console.log(theme);
+
+  const cookieStore = await  cookies();
+  cookieStore.set("resultsPerPage", "10");
+  console.log((await cookieStore).get("resultsPerPage"));
+
   return new Response('<h1>Profile API data</h1>',{
     headers:{
       'Content-Type': 'text/html',
+      "Set-Cookie":"theme=dark",
     },
 });
 }
+
+
+
+
+
+
+
+
+
+
