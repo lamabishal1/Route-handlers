b95074166c33f81a7c3c2ca0cc32c39433d8f2e5
LAMA BISHAL <lb1627565@gmail.com>
2025-08-13 21:26:22 +0900

    caching in route handlers and middleware

---
+++ middleware.js
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+
+
+export function middleware(request) {
+    const response = NextResponse.next();
+    const themePreference = request.cookies.get('theme')
+    if(!themePreference) {
+        response.cookies.set('theme', 'dark');
+        }
+
+    response.headers.set("custom-header", "my custom header");    
+    return response;
+    }
+    // return NextResponse.redirect(new URL('/', request.url));
+    // if (request.nextUrl.pathname === "/profile"){
+    //     return NextResponse.rewrite(new URL('/hello', request.url));
+    // }
+
+
+
+// export const config = {
+//     matcher: "/profile",
+// };
